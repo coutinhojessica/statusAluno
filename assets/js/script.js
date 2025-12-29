ccdc967f90abfd3e4b02345365618766b61606e6
Jéssica coutinho <jessicamaiara.coutinho@gmail.com>
2025-12-29 09:48:42 -0300

    Correção de bugs. Projeto aceitava valores negativos e muito altos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+function validacaoNota (nota) {
+      
+        if (nota <= 0 || nota > 10) {
+            return false;
+        }else{
+            return nota
+        }
+
+}
+
 function converterParaVirgula(numero) {
     if (isNaN(numero)) return "";
 
@@ -7,6 +17,8 @@ function converterParaVirgula(numero) {
 function converterParaPonto(valor) {
     if (typeof valor !== "string") {
         return NaN;
+    } else if (valor < 0){
+        return false;
     }
 
     const normalizado = valor.replace(",", ".");
@@ -14,12 +26,13 @@ function converterParaPonto(valor) {
 }
 
 function verificarMedia(n1, n2, n3, n4) {
+
     if (n1 && n2 && n3 && n4) {
         let mediaEscolar = (n1 + n2 + n3 + n4) / 4;
         return mediaEscolar;
     } else{
-        return false;
-    }
+        return false
+    } 
 }
 
 let forms = document.getElementById("forms");
@@ -33,27 +46,58 @@ forms.addEventListener("submit", (event) => {
         nota4: (four = document.getElementById("nota4").value),
     };
 
-    let notasAlunos1 = converterParaPonto(notas.nota1);
-    let notasAlunos2 = converterParaPonto(notas.nota2);
-    let notasAlunos3 = converterParaPonto(notas.nota3);
-    let notasAlunos4 = converterParaPonto(notas.nota4);
+    let n1 = validacaoNota(notas.nota1);
+    let n2 = validacaoNota(notas.nota2);
+    let n3 = validacaoNota(notas.nota3);
+    let n4 = validacaoNota(notas.nota4); 
+
+    let msgInfo = document.querySelector(".msgInfo");
+
+    if (!n1 || !n2 || !n3 || !n4) {
+         msgInfo.textContent = `As notas estão inválidas ou não foram preenchidas.`;
+    }
+   
+
+    let notasAlunos1 = converterParaPonto(n1);
+    let notasAlunos2 = converterParaPonto(n2);
+    let notasAlunos3 = converterParaPonto(n3);
+    let notasAlunos4 = converterParaPonto(n4);
+
 
     let calculoMediaEscolar = verificarMedia(notasAlunos1, notasAlunos2, notasAlunos3, notasAlunos4);
 
-    let msgResultado = document.querySelector(".msgErro");
-
-    let resultadoNotas = converterParaVirgula(calculoMediaEscolar);
-
+    let resultadoNotas = converterParaVirgula(calculoMediaEscolar.toFixed(1));
+    
     let resultado = document.querySelector(".resultado");
+
 
     if (calculoMediaEscolar) {
         if (calculoMediaEscolar >= 7) {
-            resultado.textContent = `Parabéns, você foi aprovado! Sua média foi: ${resultadoNotas}`;
+            resultado.innerHTML = `Parabéns, você foi aprovado! 
+                <br><strong>Suas notas foram:</strong> 
+                    <br>1º bimestre ${notas.nota1}, 
+                    <br>2º bimestre ${notas.nota2},
+                    <br>3º bimestre ${notas.nota3}, 
+                    <br>4º bimestre ${notas.nota4}. 
+                    <br>
+                <br><strong> Sua média foi:</strong> ${resultadoNotas}`;
+                msgInfo.textContent = "";
         } else {
-            resultado.textContent = `Infelizmente você ficou reprovado! Sua média foi: ${resultadoNotas}`;
-            msgResultado.textContent = "";
+                resultado.innerHTML = `Infelizmente você ficou reprovado! 
+                <br><strong>Suas notas foram:</strong> 
+                    <br>1º bimestre ${notas.nota1}, 
+                    <br>2º bimestre ${notas.nota2}, 
+                    <br>3º bimestre ${notas.nota3}, 
+                    <br>4º bimestre ${notas.nota4}. 
+                    <br>
+                <br> Sua média foi: ${resultadoNotas}`;
+                msgInfo.textContent = "";
         }
-    } else {
-        msgResultado.textContent = "Está faltando notas para fechar o bimestre.";
+    } else{
+        msgInfo.textContent = `As notas estão inválidas ou não foram preenchidas.`;
     }
+    
 });
+
+
+  
